2031296c0139751219ed129f2b75935dce7aa9ca
JangHwan <wkd5878@gmail.com>
2025-05-16 14:19:00 +0900

    Note : Udemy ES2015 JS class

--- JavaScript/ES2015/classExam01.js
+++ JavaScript/ES2015/classExam01.js
@@ -2,9 +2,27 @@ class Student {
   constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.score = [];
+  }
+  
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  
+  addScore(score) {
+    this.score.push(score);
+    return this.score;
+  }
+  
+  static EnrollStudents() {
+    return "Enroll Students ~ !";
   }
 }
 
 // 인스턴스 생성
 const firstStudent = new Student('Kim', 'Lee');
+console.log(firstStudent.fullName());
+console.log(firstStudent.addScore(1));
+console.log(firstStudent.addScore(2));
 console.log(firstStudent);
+console.log(Student.EnrollStudents());
